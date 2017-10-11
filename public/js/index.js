1f08f0175ff871e53465a7f7d1a87fedb1bbe9f1
Himanshu Gupta <himanshu.iitr1@gmail.com>
2017-10-11 19:35:49 +0530

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
-
 });
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -19,6 +19,17 @@ io.on('connection', (socket) => {
     console.log('User was disconnected');
   });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user has joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -26,6 +37,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 const port = process.env.PORT || 3000;
