ff35f6601a0506406d2f89924d435f12325441e3
Himanshu Gupta <himanshu.iitr1@gmail.com>
2017-10-11 18:24:46 +0530

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Andrew',
+    text: 'That works for me'
+  });
+});
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
-// console.log(publicPath);
+
 const socketIO = require('socket.io');
 const http = require('http');
 const express = require('express');
@@ -18,8 +18,16 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-});
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'Hey. Ok.',
+    createdAt: 123123
+  });
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+});
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening at port ${port}`);
